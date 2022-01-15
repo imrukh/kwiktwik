28a78bb61995804c7583091a3730023518ba042d
imrukh <shahrukh.gulbahar@walmart.com>
2022-01-15 17:28:56 +0530

    test 5

--- src/main/java/com/kwiktwik/feedbackservice/controllers/FeedbackController.java
+++ src/main/java/com/kwiktwik/feedbackservice/controllers/FeedbackController.java
@@ -21,7 +21,7 @@ import java.util.List;
 
 import static com.kwiktwik.feedbackservice.util.LoggingAction.Status.SUCCESS;
 
-@CrossOrigin(origins = "*")
+@CrossOrigin
 @RestController
 @RequestMapping(value = "/api/slot")
 public class FeedbackController {
