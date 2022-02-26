5333d2be98b1ebe3d40c9944372b199cc59b6a81
imrukh <shahrukh.gulbahar@walmart.com>
2022-02-26 23:34:47 +0530

    test

--- src/main/java/com/kwiktwik/feedbackservice/config/RestConfig.java
+++ src/main/java/com/kwiktwik/feedbackservice/config/RestConfig.java
@@ -1,26 +1,25 @@
-//package com.kwiktwik.feedbackservice.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.cors.CorsConfiguration;
-//import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-//import org.springframework.web.filter.CorsFilter;
-//
-//@Configuration
-//public class RestConfig {
-//    @Bean
-//    public CorsFilter corsFilter() {
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        CorsConfiguration config = new CorsConfiguration();
-//        config.setAllowCredentials(true);
-//        config.addAllowedOrigin("*");
-//        config.addAllowedHeader("*");
-//        config.addAllowedMethod("OPTIONS");
-//        config.addAllowedMethod("GET");
-//        config.addAllowedMethod("POST");
-//        config.addAllowedMethod("PUT");
-//        config.addAllowedMethod("DELETE");
-//        source.registerCorsConfiguration("/**", config);
-//        return new CorsFilter(source);
-//    }
-//}
+package com.kwiktwik.feedbackservice.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class RestConfig {
+
+    @Bean
+    public CORSFilter corsFilter() {
+        CorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("http://localhost:4200");
+        config.addAllowedMethod(HttpMethod.DELETE);
+        config.addAllowedMethod(HttpMethod.GET);
+        config.addAllowedMethod(HttpMethod.OPTIONS);
+        config.addAllowedMethod(HttpMethod.PUT);
+        config.addAllowedMethod(HttpMethod.POST);
+        ((UrlBasedCorsConfigurationSource) source).registerCorsConfiguration("/**", config);
+        return new CORSFilter(source);
+    }
+}
--- src/main/java/com/kwiktwik/feedbackservice/config/WebSecurityConfig.java
+++ src/main/java/com/kwiktwik/feedbackservice/config/WebSecurityConfig.java
@@ -1,32 +1,32 @@
-package com.kwiktwik.feedbackservice.config;
-
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.web.cors.CorsConfiguration;
-
-import java.util.Arrays;
-
-@EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-    CorsConfiguration configuration = new CorsConfiguration();
-    configuration.setAllowedOriginPatterns(Arrays.asList("*"));
-    configuration.setAllowedMethods(Arrays.asList("GET", "POST", "OPTIONS", "DELETE", "PUT", "PATCH"));
+//package com.kwiktwik.feedbackservice.config;
+//
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.web.cors.CorsConfiguration;
+//
+//import java.util.Arrays;
+//
+//@EnableWebSecurity
+//public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//    CorsConfiguration configuration = new CorsConfiguration();
+//    configuration.setAllowedOriginPatterns(Arrays.asList("*"));
+//    configuration.setAllowedMethods(Arrays.asList("GET", "POST", "OPTIONS", "DELETE", "PUT", "PATCH"));
 //    configuration.setAllowedHeaders(Arrays.asList("X-Requested-With", "Origin", "Content-Type", "Accept", "Authorization",
-//            "sec-ch-ua-platform",
+//            "sec-ch-ua-platform","",
 //            "User-Agent",
 //            "Referer","Content-Type", "sec-ch-ua-mobile", "sec-ch-ua"
 //            ));
-        //        config.addAllowedOrigin("*");
-        configuration.addAllowedHeader("*");
-    configuration.setAllowCredentials(true);
-
-        // You can customize the following part based on your project, it's only a sample
-        http.authorizeRequests().antMatchers("/**").permitAll().anyRequest()
-                .authenticated().and().csrf().disable().cors().configurationSource(request -> configuration);
-
-    }
-}
+//        configuration.addAllowedOrigin("*");
+////        configuration.addAllowedHeader("*");
+//    configuration.setAllowCredentials(true);
+//
+//        // You can customize the following part based on your project, it's only a sample
+//        http.authorizeRequests().antMatchers("/**").permitAll().anyRequest()
+//                .authenticated().and().csrf().disable().cors().configurationSource(request -> configuration);
+//
+//    }
+//}
